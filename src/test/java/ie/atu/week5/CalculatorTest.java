7adb913480d8e2f49b6329ae96a23fd9f79fbbc3
Patryk Milkiewicz <g00397265@atu.ie>
2022-10-19 14:30:37 +0100

    Added Test for adding and an adding class

---
+++ src/test/java/ie/atu/week5/CalculatorTest.java
@@ -0,0 +1,26 @@
+package ie.atu.week5;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    Calculator myCalc;
+
+    @BeforeEach
+    void setUp() {
+        myCalc = new Calculator();
+    }
+
+    @Test
+    void addTest(){
+        assertEquals(3, myCalc.add(1, 2));
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+}
